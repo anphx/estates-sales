c0e6c25171419cc424c05f62676c1aa7080ac29d
An Pham <ptan1991@gmail.com>
2016-02-21 20:27:59 +0700

    Add more images.

--- back-end/db/seeds.rb
+++ back-end/db/seeds.rb
@@ -20,7 +20,14 @@ image_list = [
   "http://ap.rdcpix.com/1496344611/a78b1c7c212722d0eeaaeb390b31cc33l-m1xd-w640_h480_q80.jpg",
   "http://ap.rdcpix.com/142029107/c3d5b8c65584d753a1a3a35898451bedl-m0xd-w640_h480_q80.jpg",
   "http://ap.rdcpix.com/1176702658/c3d5b8c65584d753a1a3a35898451bedl-m1xd-w640_h480_q80.jpg",
-  "http://ap.rdcpix.com/1811970413/c3d5b8c65584d753a1a3a35898451bedl-m2xd-w640_h480_q80.jpg"
+  "http://ap.rdcpix.com/1811970413/c3d5b8c65584d753a1a3a35898451bedl-m2xd-w640_h480_q80.jpg",
+  "http://ap.rdcpix.com/890873050/50fbf775783637910adfb4032372dcf8l-m0xd-w480_h480_q80.jpg",
+  "http://ap.rdcpix.com/1262874791/d80856151787d934771c1f69e829211al-m0xd-w480_h480_q80.jpg",
+  "http://ap.rdcpix.com/781413899/9ab6fec6a2edf045dc45a05cfd4e3a02l-m0xd-w480_h480_q80.jpg",
+  "http://ap.rdcpix.com/985702607/6e3302e57eb5586a0b8369e3df4a3e23l-m0xd-w480_h480_q80.jpg",
+  "http://ap.rdcpix.com/373773908/2f4a971ab31bbfafa258d4df650ded40l-m0xd-w480_h480_q80.jpg",
+  "http://ap.rdcpix.com/735106764/5fbfdc648e735b997c047087cd93a20al-m0xd-w480_h480_q80.jpg",
+  "http://ap.rdcpix.com/785002956/4fa09a2ee35e52f84b026696f715411fl-m0xd-w480_h480_q80.jpg"
 ]
 
 image_urls = [].tap do |array|
